4f72160866099cbc59fe832489cdf8f8e43d4476
Iqbal-Nurhaq <iqbalnur2009@gmail.com>
2024-10-21 14:38:37 +0700

    Admin Home, Admin Page Login
    Page admin home dan page login final

--- Admin.js
+++ Admin.js
@@ -1,20 +1,45 @@
+var Username;
+var Password;
+var isRegistered = false;
+var arrUser = [];
+var arrPass = [];
 function HideLogin(){
-    var Username = $("#Username").val();
-    var Password = $("#Password").val();
-    
-    if(Username != "admin" && Password != "#123"){
+    Username = $("#Username").val();
+    Password = $("#Password").val();
+    for(var i = 0; i < arrUser.length; i++){
+        console.log(arrUser[i], arrPass[i]);
+        console.log(Username, Password)
+        if(arrUser[i] == Username && arrPass[i] == Password){
+            isRegistered = true;
+
+        }
+    }
+
+    if(!isRegistered){
         alert("PASSWORD DAN USERNAME SALAH!");
     }else{
         alert("Login Berhasil!");
-        $(".SquareLogin").remove();
-        $(".SquareMenu").removeAttr("hidden");
+        window.location = 'CRUDMenu.html';
     }
 }
 
-function RedirectToMitra(){
+$(document).ready(function(){
+    loadJSON();
+})
 
+
+function loadJSON(){
+    $.getJSON('Admin.json', function(data){
+        for(var i in data["AdminAccount"]){
+            arrUser.push(data["AdminAccount"][i]["username"]);
+            arrPass.push(data["AdminAccount"][i]["password"]);
+        }
+        
+    });
 }
 
-function RedirectToObat(){
-    
+function redirectToLogin(){
+    window.location = 'LoginAdmin.html';
+    alert("Logout!");
 }
+
